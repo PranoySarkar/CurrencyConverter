7507e8e5e3b1ef4234b42badf63dff2c22968fc7
Pranoy Sarkar <pranoy.sarkar@softwareag.com>
2020-05-22 21:25:05 +0530

    Added 103 class

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-alert("Hello world")
+
+/*
+function foo(){
+    let a=1;
+    let b=2;
+    let c=a+b;
+    console.log(c);
+}
+
+foo()
+foo()*/
+
+/* let inrValue = document.querySelector('input');
+console.log(inrValue.value); */
+
+let btn = document.querySelector('button');
+btn.addEventListener('click',buttonClicked);
+
+function buttonClicked(){
+    console.log('Button clicked')
+}
